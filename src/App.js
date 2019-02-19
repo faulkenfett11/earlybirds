e2d248858f1bdce3c5b86a52069e4a65df14ec70
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2019-02-19 01:20:12 -0600

    ebcb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./pictures/Early Birds2.png";
+import {
+  FaTrademark,
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagram
+} from "react-icons/fa";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <h1 className="comingSoon">Coming Soon to Austin!</h1>
+        <img src={logo} className="earlyBirdsLogo" alt="earlyBirdsLogo" />
+        <h5 className="underConstruction">
+          While our website is under construction, check out our social media:
+        </h5>
+        <div className="socialMedia">
+          <a href="https://www.facebook.com/earlybirdsatx/" target="_blank" rel="noopener noreferrer">
+            <FaFacebookSquare />
           </a>
-        </header>
+          <a href="https://www.twitter.com/earlybirdsatx/" target="_blank" rel="noopener noreferrer">
+            <FaTwitterSquare />
+          </a>
+          <a href="https://www.instagram.com/earlybirdsatx/" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+        </div>
+        <footer>
+          <h5>
+            2019 Early Birds Coffee Bikes <FaTrademark />
+          </h5>
+        </footer>
       </div>
     );
   }
